e121a3c82b33eb9435a7f22bc96d49e6711dc0ed
Mfam <mfamthankgod70@gmail.com>
2025-09-05 12:08:44 -0700

    i added the production url

--- Backend/Server.js
+++ Backend/Server.js
@@ -19,7 +19,7 @@ app.use(express.json({limit:"10mb"}))
 app.use(cookieParser())
 const __dirname=path.resolve()
 app.use(cors({
-  origin:process.env.NODE_ENV?"http://localhost:5173":"/api",
+  origin:process.env.NODE_ENV?"http://localhost:5173":"https://buysphere.onrender.com/api",
   credentials:true
 }))
 app.use("/api/auth",authrouter)
