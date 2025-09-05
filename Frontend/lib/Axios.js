fab264a051d16ef7fcbe9a2f249574399cd74443
Mfam <mfamthankgod70@gmail.com>
2025-09-05 07:21:28 -0700

    i deleted yaml file and also change the baseur configuration

--- Frontend/lib/Axios.js
+++ Frontend/lib/Axios.js
@@ -1,7 +1,6 @@
 import axios from "axios"
 const AxiosInstance=axios.create({
-//    baseURL:import.meta.mode==="development"? "http://localhost:5000/api":"/api",
-    baseURL: "http://localhost:5000/api",
+   baseURL:import.meta.env.MODE==="development"? "http://localhost:5000/api":"/api",
     withCredentials:true,//this allows us to send cookies in the request
 })
 export default AxiosInstance
