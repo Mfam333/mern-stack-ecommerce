54fc53eec7d7a0de799b9a2a38fcbcc495e6d775
Mfam <mfamthankgod70@gmail.com>
2025-09-05 09:50:55 -0700

    changes 1

--- Frontend/lib/Axios.js
+++ Frontend/lib/Axios.js
@@ -1,8 +1,14 @@
-import axios from "axios"
-const AxiosInstance=axios.create({
-   baseURL:import.meta.env.MODE==="development"?
-    "http://localhost:5000/api"
-    :"https://buysphere.onrender.com/api",
-    withCredentials:true,//this allows us to send cookies in the request
-})
-export default AxiosInstance
+import axios from "axios";
+
+const AxiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "/api",
+  withCredentials: true, // this allows us to send cookies in the request
+});
+
+export default AxiosInstance;
+
+
+
