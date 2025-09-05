5bb83f1cb9fe3a24f4fe8f177ada2c23d4a86605
Mfam <mfamthankgod70@gmail.com>
2025-09-05 09:42:18 -0700

    url change

--- Frontend/lib/Axios.js
+++ Frontend/lib/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 const AxiosInstance=axios.create({
    baseURL:import.meta.env.MODE==="development"?
     "http://localhost:5000/api"
-    :"https://https://buysphere.onrender.com/api",
+    :"https://buysphere.onrender.com/api",
     withCredentials:true,//this allows us to send cookies in the request
 })
 export default AxiosInstance
