9d6487bbb07213ba6a063143cf4c0277939477e2
Mfam <mfamthankgod70@gmail.com>
2025-09-05 10:19:19 -0700

    change in Three

--- Frontend/lib/Axios.js
+++ Frontend/lib/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const AxiosInstance = axios.create({
   baseURL:
-    import.meta.env.MODE === "development"
+    import.meta.env.MODE==="development"
       ? "http://localhost:5000/api"
       : "/api",
   withCredentials: true, // this allows us to send cookies in the request
