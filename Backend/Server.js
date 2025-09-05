453e7f447f96d58560277fd0e5edefd764272eb0
Mfam <mfamthankgod70@gmail.com>
2025-09-05 12:40:42 -0700

    changes on the frontend and backend url

--- Backend/Server.js
+++ Backend/Server.js
@@ -19,7 +19,7 @@ app.use(express.json({limit:"10mb"}))
 app.use(cookieParser())
 const __dirname=path.resolve()
 app.use(cors({
-  origin:process.env.NODE_ENV?"http://localhost:5173":"https://buysphere.onrender.com/api",
+  origin:"http://localhost:5173",
   credentials:true
 }))
 app.use("/api/auth",authrouter)
--- Frontend/lib/Axios.js
+++ Frontend/lib/Axios.js
@@ -4,7 +4,7 @@ const AxiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE==="development"
       ? "http://localhost:5000/api"
-      : "https://buysphere.onrender.com/api",
+      : "api",
   withCredentials: true, // this allows us to send cookies in the request
 });
 
