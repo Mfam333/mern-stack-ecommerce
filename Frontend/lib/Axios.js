4a4a49583e6712c9b1758c01ba5c6c6b1010b977
Mfam <mfamthankgod70@gmail.com>
2025-09-05 23:54:12 -0700

    baseurl was configured

--- Frontend/lib/Axios.js
+++ Frontend/lib/Axios.js
@@ -4,7 +4,7 @@ const AxiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE==="development"
       ? "http://localhost:5000/api"
-      : "api",
+      : "/api",
   withCredentials: true, // this allows us to send cookies in the request
 });
 
