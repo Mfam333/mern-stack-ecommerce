18c18e4e0daff2b2d7c9bc1dba1691de7604d9c3
Mfam <mfamthankgod70@gmail.com>
2025-09-06 00:19:26 -0700

    change was made on the backend to resolve cannot get error

--- Backend/Server.js
+++ Backend/Server.js
@@ -31,12 +31,11 @@ app.use("/api/cart",cartrouter)
 app.use("/api/coupons",couponrouter)
 app.use("/api/payments",Paymentrouter)
 app.use("/api/analytics",Analyticrouter)
-if(process.env.NODE_ENV==="production"){
+
   app.use(express.static(path.join(__dirname,"../Frontend/dist")))
   app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"../Frontend/dist/index.html"))
   })
-}
 
 
 
